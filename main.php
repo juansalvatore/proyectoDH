20b3af19e4ea0292e90abf4cf816281edd9f3908
Juan Salvatore <juansalvatore@live.com.ar>
2017-09-24 21:55:09 -0300

    Create main. Add 3 icons

---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main-style.min.css">
+    <title>Boole</title>
+  </head>
+  <body>
+    <!-- Left navbar -->
+    <div class="navbar">
+      <div class="proyectos">
+        <img src="images/oval.png" alt="">
+      </div>
+      <div class="tresBotones">
+        <div class="button plus">
+          <img src="images/plus.png" alt="">
+        </div>
+        <div class="button info">
+          <img src="images/information.png" alt="">
+        </div>
+        <div class="button user">
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
